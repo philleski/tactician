b0b9cd1580fbf0fb5f73a9294a078bbfb3040b58
philleski <philleski@gmail.com>
2015-06-11 01:44:50 -0700

    Putting in weights according to pawn position. Also first version of deep capture probing.

--- src/chess_engine/Brain.java
+++ src/chess_engine/Brain.java
@@ -14,7 +14,8 @@ public class Brain {
 		return count;
 	}
 	
-	private boolean isEndgame(Board board) {
+	private float endgameFraction(Board board) {
+		// Returns 0 if it's the start of the game, 1 if it's just two kings.
 		int blackBishopCount = numBitsSet(board.blackBishops);
 		int blackKnightCount = numBitsSet(board.blackKnights);
 		int blackPawnCount = numBitsSet(board.blackPawns);
@@ -38,13 +39,10 @@ public class Brain {
 			whitePawnCount * this.FITNESS_PAWN +
 			whiteQueenCount * this.FITNESS_QUEEN +
 			whiteRookCount * this.FITNESS_ROOK;
-		if(blackMaterial <= 1300 && whiteMaterial <= 1300) {
-			return true;
-		}
-		return false;
+		return (blackMaterial + whiteMaterial) / (2 * this.FITNESS_START_NOKING);
 	}
 	
-	private float fitness(Board board) {
+	private float fitnessFast(Board board) {
 		int blackBishopCount = numBitsSet(board.blackBishops);
 		int blackKingCount = numBitsSet(board.blackKings);
 		int blackKnightCount = numBitsSet(board.blackKnights);
@@ -83,11 +81,89 @@ public class Brain {
 		
 		return fitness;
 	}
+	
+	private float fitnessAccurate(Board board) {
+		int blackBishopCount = numBitsSet(board.blackBishops);
+		int blackKingCount = numBitsSet(board.blackKings);
+		int blackKnightCount = numBitsSet(board.blackKnights);
+		int blackQueenCount = numBitsSet(board.blackQueens);
+		int blackRookCount = numBitsSet(board.blackRooks);
+		int whiteBishopCount = numBitsSet(board.whiteBishops);
+		int whiteKingCount = numBitsSet(board.whiteKings);
+		int whiteKnightCount = numBitsSet(board.whiteKnights);
+		int whiteQueenCount = numBitsSet(board.whiteQueens);
+		int whiteRookCount = numBitsSet(board.whiteRooks);
+				
+		float blackMaterial =
+			blackBishopCount * this.FITNESS_BISHOP +
+			blackKingCount * this.FITNESS_KING +
+			blackKnightCount * this.FITNESS_KNIGHT +
+			blackQueenCount * this.FITNESS_QUEEN +
+			blackRookCount * this.FITNESS_ROOK;
+		float whiteMaterial =
+			whiteBishopCount * this.FITNESS_BISHOP +
+			whiteKingCount * this.FITNESS_KING +
+			whiteKnightCount * this.FITNESS_KNIGHT +
+			whiteQueenCount * this.FITNESS_QUEEN +
+			whiteRookCount * this.FITNESS_ROOK;
+		
+		float endgameFraction = this.endgameFraction(board);
+		
+		for(int rank = 1; rank < 7; rank++) {   // Since pawns can't be on the edge ranks.
+			for(int centrality = 0; centrality < 4; centrality++) {
+				// Magic number: a1-h1
+				long rankMaskWhite = 0x00000000000000ffL << (rank * 8);
+				// Magic number: a8-h8
+				long rankMaskBlack = 0xff00000000000000L >>> (rank * 8);
+				// Magic number: a1-a8
+				long centralityMask = (0x0101010101010101L << centrality) | (0x0101010101010101L << (8 - centrality));
+				
+				float pawnFactor = (1 - endgameFraction) * this.FITNESS_PAWN_TABLE_OPENING[rank][centrality] +
+					endgameFraction * this.FITNESS_PAWN_TABLE_ENDGAME[rank][centrality];
+								
+				blackMaterial += pawnFactor *
+					numBitsSet(board.blackPawns & rankMaskBlack & centralityMask);
+				whiteMaterial += pawnFactor *
+					numBitsSet(board.whitePawns & rankMaskWhite & centralityMask);
+			}
+		}
+		
+		float fitness = 0;
+		if(board.turn == board.WHITE) {
+			fitness = whiteMaterial - blackMaterial;
+		}
+		else {
+			fitness = blackMaterial - whiteMaterial;
+		}
+		
+		return fitness;
+	}
+	
+	private float probeCapture(Board board, long target) {
+		ArrayList<long[]> lmf = board.legalMovesFast();
+		float bestFitness = this.fitnessAccurate(board);
+		for(long[] move : lmf) {
+			if(move[1] != target) {
+				continue;
+			}
+			Board copy = new Board(board);
+			try {
+				copy.move(move);
+			}
+			catch(IllegalMoveException e) {
+			}
+			float candidate = -this.probeCapture(copy, target);
+			if(candidate > bestFitness) {
+				bestFitness = candidate;
+			}
+		}
+		return bestFitness;
+	}
 
 	// http://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
 	private float alphabeta(Board board, int depth, float alpha, float beta) {
 		if(depth == 0) {
-			return this.fitness(board);
+			return this.fitnessAccurate(board);
 		}
 		float superlativeFitness = 0;
 		if(depth % 2 == 0) {
@@ -96,7 +172,7 @@ public class Brain {
 		else {
 			superlativeFitness = FITNESS_LARGE;
 		}
-		
+				
 		// Check for stalemate or checkmate.
 		ArrayList<long[]> lmf = board.legalMovesFast();
 		if(lmf.size() <= 8) {
@@ -127,7 +203,16 @@ public class Brain {
 			}
 			catch(IllegalMoveException e) {
 			}
-			float fitness = this.alphabeta(copy, depth - 1, alpha, beta);
+			float fitness = 0;
+			if(depth == 1 && (move[1] & copy.allPieces) != 0) {
+				// This is to deal with the scenario where say the queen
+				// captures a heavily guarded pawn right when the depth
+				// expires.
+				fitness = this.probeCapture(copy, move[1]);
+			}
+			else {
+				fitness = this.alphabeta(copy, depth - 1, alpha, beta);
+			}
 			if(depth % 2 == 0 && fitness > superlativeFitness) {
 				superlativeFitness = fitness;
 				if(superlativeFitness > alpha) {
@@ -152,8 +237,10 @@ public class Brain {
 	
 	public long[] getMove(Board board) {
 		int depth = 0;
-		if(this.isEndgame(board)) {
-			depth = 7;
+		float endgameFraction = this.endgameFraction(board);
+		System.out.println("EF: " + endgameFraction);
+		if(endgameFraction > 0.3) {
+			depth = 5;
 		}
 		else {
 			depth = 6;
@@ -169,6 +256,7 @@ public class Brain {
 			superlativeFitness = FITNESS_LARGE;
 		}
 		for(long[] move : board.legalMoves()) {
+			String ma = board.moveToAlgebraic(move);
 			Board copy = new Board(board);
 			try {
 				copy.move(move);
@@ -176,6 +264,8 @@ public class Brain {
 			catch(IllegalMoveException e) {
 			}
 			float fitness = this.alphabeta(copy, depth - 1, -FITNESS_LARGE, FITNESS_LARGE);
+			String message = ma + ": " + fitness;
+			// System.out.println(message);
 			fitness += Math.random() * 0.01;
 			if(depth % 2 == 0 && fitness > superlativeFitness) {
 				superlativeFitness = fitness;
@@ -197,4 +287,31 @@ public class Brain {
 	private float FITNESS_PAWN = 100;
 	private float FITNESS_QUEEN = 880;
 	private float FITNESS_ROOK = 510;
+	private float FITNESS_START_NOKING = 2 * FITNESS_ROOK +
+		2 * FITNESS_KNIGHT + 2 * FITNESS_BISHOP + FITNESS_QUEEN +
+		8 * FITNESS_PAWN;
+	
+	// It goes as [rank][centrality]. rank goes from 0 to 7 and is from the
+	// perspective of that player. centrality goes from 0 (files a, h) to 3
+	// (files d, e).
+	private float[][] FITNESS_PAWN_TABLE_OPENING = {
+		{0, 0, 0, 0},
+		{90, 95, 105, 110},
+		{90, 95, 105, 115},
+		{90, 95, 110, 120},
+		{97, 103, 117, 127},
+		{106, 112, 125, 140},
+		{117, 122, 134, 159},
+		{0, 0, 0, 0}
+	};
+	private float[][] FITNESS_PAWN_TABLE_ENDGAME = {
+		{0, 0, 0, 0},
+		{120, 105, 95, 90},
+		{120, 105, 95, 90},
+		{125, 110, 100, 95},
+		{133, 117, 107, 100},
+		{145, 129, 116, 105},
+		{161, 146, 127, 110},
+		{0, 0, 0, 0}
+	};
 }
